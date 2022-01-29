8d766cf6bacfd38b68ebbe7e0ae0cb1c41d52acd
Elijah Meshnick <emeshnick@gmail.com>
2022-01-29 10:35:06 -0700

    About page edit

--- client/components/About.js
+++ client/components/About.js
@@ -12,9 +12,10 @@ class About extends React.Component {
       <Container style={styles.mainContainer}>
         <h1>About This App</h1>
         <p>
-          This app uses basic webscraping to scan through the links on a given
-          page. If there is an error reaching the page, it is added to the list
-          of broken links with the URL and the text that it is connected to.{" "}
+          This app uses basic webscraping to scan through the links on any given
+          page. If the server finds an error in trying to reach one of the
+          scanned links, it is added to the list of broken links. Broken links
+          are displayed by the URL and the text that it is connected to.{" "}
           <br></br>
           You can find the repository for this project on{" "}
           <a href="https://github.com/emeshnick/broken-link-finder">Github</a>.
