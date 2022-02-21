c9bae8cff9b5658f98858f6c83f94235997b3a11
Elijah Meshnick <emeshnick@gmail.com>
2022-02-21 16:15:27 -0500

    Form submits on enter keypress

--- public/bundle.js
+++ public/bundle.js
@@ -773,7 +773,6 @@ var reducer = Object(redux__WEBPACK_IMPORTED_MODULE_0__["combineReducers"])({
 var middleware = Object(redux_devtools_extension__WEBPACK_IMPORTED_MODULE_3__["composeWithDevTools"])(Object(redux__WEBPACK_IMPORTED_MODULE_0__["applyMiddleware"])(redux_thunk__WEBPACK_IMPORTED_MODULE_2__["default"], Object(redux_logger__WEBPACK_IMPORTED_MODULE_1__["createLogger"])({
   collapsed: true,
   predicate: function predicate() {
-    // return true
     return false;
   }
 })));
